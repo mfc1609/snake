e98220661763ea2761c32bcc4ace588d3fc74e5f
Toan <tkj@DESKTOP-CKRVQLT.>
2024-10-21 01:19:13 +0200

    Q1 + Q2/Q3 partiel

---
+++ Partie4.s
@@ -0,0 +1,100 @@
+.data
+	largeur_pixel:   .word 240
+	hauteur_pixel:	 .word 240
+	largeur_unit:    .word 8
+	hauteur_unit:	 .word 8
+	
+	tete: 	.word 0
+	queue: 	.word 0
+	max: 	.word 900
+	
+.text
+
+main:
+	jal F_creer
+	
+	la t0, tete        
+    	sw a0, 0(t0)       
+    
+  	la t1, queue        
+    	sw a0, 0(t1)
+    	
+	li a1 10
+	jal F_enfiler
+	jal F_enfiler
+	jal F_enfiler
+	jal F_defiler
+	
+exit:
+	li a7 10
+	ecall
+
+	
+I_largeur:
+	la t0 largeur_pixel
+	lw t1, 0(t0)
+	la t2 largeur_unit
+	lw t3, 0(t2)
+	div a0 t1 t3
+	jr ra
+		
+		
+I_hauteur:
+	la a1 hauteur_pixel
+	lw a2, 0(a1)
+	la a3 hauteur_unit
+	lw a4, 0(a3)
+	div a0 a2 a4
+	jr ra	
+
+F_creer:
+	addi sp sp -4
+	sw ra (sp)
+	
+	jal I_largeur
+	mv t1 a0
+	jal I_hauteur
+	mv t2 a0
+	mul a0 t1 t2
+	slli a0 a0 2
+	li a7 9
+	ecall
+	
+	lw ra (sp)
+	addi sp sp 4
+	jr ra
+	
+F_enfiler:
+	
+	la t0 tete
+	lw t1 0(t0)
+	lw t2 max
+	
+	# Ajoute le pixel 
+	sw a1 0(a0)
+	addi a0 a0 4
+	
+	#incr
+	addi t1 t1 4
+	sw t1 (t0)
+	
+	jr ra
+	
+F_defiler:
+
+	la t0 queue
+	lw t1 0(t0)
+	lw t2 max
+	
+	addi t1 t1 4
+	sw t1 (t0)
+	
+	jr ra
+	
+	
+	
+	
+	
+	
+	
+	
